35cffc19e3a98257a607079a8e668e13989834ad
Dilip Mayavanshi <dilip@Dilips-MacBook-Air.local>
2026-05-19 14:17:10 +0530

    fixed urls

--- server/urls.py
+++ server/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from turtle import home
+
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path , include

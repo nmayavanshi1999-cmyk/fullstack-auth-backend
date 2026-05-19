7a930fd78526aac0047a8b0708eb806c71f706e0
Dilip Mayavanshi <dilip@Dilips-MacBook-Air.local>
2026-05-19 12:56:21 +0530

    added home route

--- server/urls.py
+++ server/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView
 from drf_spectacular.views import SpectacularAPIView , SpectacularSwaggerView
 from accounts.views import SignupView
 
-
+def home(request):
+    return HttpResponse("Backend Running Successfully")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
      path('api/', include('accounts.urls')),
     path('api/login/',TokenObtainPairView.as_view()),
